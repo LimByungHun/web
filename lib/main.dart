e1ae188ecca94eed83508256fd3ace25be807762
Jung <pearl9356@gmail.com>
2025-07-18 17:52:36 +0900

    go_router 패키지 main에 적용

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:sign_web/model/course_model.dart';
@@ -9,6 +10,7 @@ import 'package:sign_web/service/token_storage.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   final refreshToken = await TokenStorage.getRefreshToken();
   bool isLoggedIn = false;
 
@@ -25,23 +27,36 @@ void main() async {
   );
 }
 
+// 라우터 정의 (로그인 여부에 따라 초기경로 변경)
+GoRouter createRouter(bool isLoggedIn) {
+  return GoRouter(
+    initialLocation: isLoggedIn ? '/home' : '/',
+    routes: [
+      GoRoute(path: '/', builder: (context, state) => const LoginScreen()),
+      GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
+    ],
+  );
+}
+
 class MyApp extends StatelessWidget {
   final bool isLoggedIn;
   const MyApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    final router = createRouter(isLoggedIn);
+
+    return MaterialApp.router(
       title: '수어 학습 앱',
       debugShowCheckedModeBanner: false,
-      locale: Locale('ko', 'KR'),
-      supportedLocales: [Locale('ko', 'KR'), Locale('en', 'US')],
-      localizationsDelegates: [
+      locale: const Locale('ko', 'KR'),
+      supportedLocales: const [Locale('ko', 'KR'), Locale('en', 'US')],
+      localizationsDelegates: const [
         GlobalMaterialLocalizations.delegate,
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate,
       ],
-      home: isLoggedIn ? HomeScreen() : LoginScreen(),
+      routerConfig: router,
     );
   }
 }
