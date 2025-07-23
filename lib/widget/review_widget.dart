54e99bddddd0f6134b6c70991b5e7c06012f980b
Jung <pearl9356@gmail.com>
2025-07-23 13:43:16 +0900

    4차

--- lib/widget/review_widget.dart
+++ lib/widget/review_widget.dart
@@ -3,7 +3,6 @@ import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
 import 'package:sign_web/model/course_model.dart';
 import 'package:sign_web/widget/alllist_widget.dart';
-import 'package:sign_web/widget/quiz_widget.dart';
 
 class ReviewCard extends StatelessWidget {
   const ReviewCard({super.key});
@@ -82,16 +81,12 @@ class ReviewCard extends StatelessWidget {
                 ElevatedButton(
                   style: buttonStyle,
                   onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (_) => AlllistWidget(
-                          title: '복습할 수 있는 코스',
-                          courseWordsMap: courseWordsMap,
-                        ),
-                      ),
+                    GoRouter.of(context).push(
+                      '/review_all',
+                      extra: {'courseWordsMap': courseWordsMap},
                     );
                   },
+
                   child: const Text('전체 보기'),
                 ),
               ],
